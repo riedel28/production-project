d4a423df00c469f208880ab9302fcc88a78afd75
Sergey Riedel <sergey.riedel@gmail.com>
2023-01-29 13:50:02 +0100

    chore(webpack): decompose webpack config

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,25 @@
+import { Configuration } from 'webpack';
+
+import { BuildOptions } from './types/config';
+import { buildPlugins } from './buildPlugins';
+import { buildLoaders } from './buildLoaders';
+import { buildResolvers } from './buildResolvers';
+
+export function buildWebpackConfig(options: BuildOptions): Configuration {
+  const { mode, paths } = options;
+
+  return {
+    mode,
+    entry: paths.entry,
+    output: {
+      filename: '[name].[contenthash].js',
+      path: paths.build,
+      clean: true
+    },
+    plugins: buildPlugins(options),
+    module: {
+      rules: buildLoaders()
+    },
+    resolve: buildResolvers()
+  };
+}
