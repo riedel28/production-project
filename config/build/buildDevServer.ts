e086f6074a52156d6db70a3bfc0b561b921b96a0
Sergey Riedel <sergey.riedel@gmail.com>
2023-01-29 14:34:12 +0100

    chore(webpack): configure webpack-dev-server

---
+++ config/build/buildDevServer.ts
@@ -0,0 +1,12 @@
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
+
+import { BuildOptions } from './types/config';
+
+export const buildDevServer = (
+  options: BuildOptions
+): WebpackDevServerConfiguration => {
+  return {
+    port: options.port,
+    open: true
+  };
+};
--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -4,9 +4,10 @@ import { BuildOptions } from './types/config';
 import { buildPlugins } from './buildPlugins';
 import { buildLoaders } from './buildLoaders';
 import { buildResolvers } from './buildResolvers';
+import { buildDevServer } from './buildDevServer';
 
 export function buildWebpackConfig(options: BuildOptions): Configuration {
-  const { mode, paths } = options;
+  const { mode, paths, isDev } = options;
 
   return {
     mode,
@@ -20,6 +21,8 @@ export function buildWebpackConfig(options: BuildOptions): Configuration {
     module: {
       rules: buildLoaders()
     },
-    resolve: buildResolvers()
+    resolve: buildResolvers(),
+    devtool: isDev ? 'inline-source-map' : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined
   };
 }
