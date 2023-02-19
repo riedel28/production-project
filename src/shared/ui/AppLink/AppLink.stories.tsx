48aba38d169a19ae23f07c62fae0a04872ce36c0
Sergey Riedel <sergey.riedel@gmail.com>
2023-02-19 14:54:18 +0100

    chore: fix stories

--- src/shared/ui/AppLink/AppLink.stories.tsx
+++ src/shared/ui/AppLink/AppLink.stories.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-
-import { RouterDecorator } from 'shared/config/storybook/RouterDecorator/RouterDecorator';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
 import { AppLink, AppLinkTheme } from './AppLink';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'shared/AppLink',
   component: AppLink,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' }
   },
@@ -17,21 +14,32 @@ export default {
   }
 } as ComponentMeta<typeof AppLink>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof AppLink> = (args) => { return <AppLink {...args} />; };
+const Template: ComponentStory<typeof AppLink> = (args) => {
+  return <AppLink {...args} />;
+};
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   children: 'Text',
   theme: AppLinkTheme.PRIMARY
 };
-Primary.decorators = [RouterDecorator];
 
 export const Secondary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Secondary.args = {
   children: 'Text',
   theme: AppLinkTheme.SECONDARY
 };
-Secondary.decorators = [RouterDecorator];
+
+export const PrimaryDark = Template.bind({});
+PrimaryDark.args = {
+  children: 'Text',
+  theme: AppLinkTheme.PRIMARY
+};
+PrimaryDark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const SecondaryDark = Template.bind({});
+SecondaryDark.args = {
+  children: 'Text',
+  theme: AppLinkTheme.SECONDARY
+};
+SecondaryDark.decorators = [ThemeDecorator(Theme.DARK)];
--- src/widgets/Navbar/ui/Navbar.stories.tsx
+++ src/widgets/Navbar/ui/Navbar.stories.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { RouterDecorator } from 'shared/config/storybook/RouterDecorator/RouterDecorator';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
 import { Navbar } from './Navbar';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'widget/Navbar',
   component: Navbar,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' }
   }
 } as ComponentMeta<typeof Navbar>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Navbar> = (args) => { return <Navbar {...args} />; };
+const Template: ComponentStory<typeof Navbar> = (args) => {
+  return <Navbar {...args} />;
+};
 
 export const Light = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Light.args = {
-  children: 'Button'
-};
-Light.decorators = [RouterDecorator];
+Light.args = {};
+
+export const Dark = Template.bind({});
+Dark.args = {};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
