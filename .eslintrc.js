be60d8b6e319ad2694c359144f037c9105b268e9
Sergey Riedel <sergey.riedel@gmail.com>
2023-02-21 20:02:38 +0100

    chore: add react hooks plugin for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  plugins: ['react', '@typescript-eslint', 'i18next'],
+  plugins: ['react', '@typescript-eslint', 'i18next', 'react-hooks'],
   rules: {
     'react/jsx-indent': [2, 2],
     'react/jsx-indent-props': [2, 2],
@@ -50,7 +50,11 @@ module.exports = {
     semi: ['error', 'always'],
     'arrow-body-style': ['error', 'always'],
     'comma-dangle': ['error', 'never'],
-    'object-curly-newline': 'off'
+    'object-curly-newline': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error'
   },
   globals: {
     __IS_DEV__: true
