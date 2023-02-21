74181e2fd0b566b1baa99cd7f95fdf707fcd2955
Sergey Riedel <sergey.riedel@gmail.com>
2023-02-21 20:01:57 +0100

    feat: create modal component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,11 @@
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 
+import { cx } from 'shared/lib/cx';
+import { Button } from 'shared/ui/Button/Button';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
+import { Modal } from 'shared/ui/Modal/Modal';
 import { useTheme } from 'app/providers/ThemeProvider';
-import { cx } from 'shared/lib/cx';
 import { AppRouter } from './router';
 
 import './styles/index.scss';
@@ -11,10 +13,27 @@ import './styles/index.scss';
 export const App = () => {
   const { theme } = useTheme();
 
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
     <div className={cx('app', {}, [theme])}>
       <Suspense fallback="">
         <Navbar />
+        <Button
+          onClick={() => {
+            setIsOpen(true);
+          }}
+        >
+          123
+        </Button>
+        <Modal
+          isOpen={isOpen}
+          onClose={() => {
+            setIsOpen(!isOpen);
+          }}
+        >
+          123
+        </Modal>
         <div className="content-page">
           <Sidebar />
           <AppRouter />
---
+++ src/shared/ui/Modal/Modal.tsx
@@ -0,0 +1,86 @@
+import {
+  FC,
+  MouseEvent,
+  ReactNode,
+  useState,
+  useRef,
+  useEffect,
+  useCallback
+} from 'react';
+
+import { cx } from 'shared/lib/cx';
+
+import cls from './Modal.module.scss';
+
+interface ModalProps {
+  className?: string;
+  children?: ReactNode;
+  isOpen?: boolean;
+  onClose?: () => void;
+}
+
+const ANIMATION_DURATION = 300;
+
+export const Modal: FC<ModalProps> = (props) => {
+  const { className, children, isOpen, onClose } = props;
+
+  const [isClosing, setIsClosing] = useState<boolean>(false);
+  const timeoutRef = useRef<number | null>(null);
+
+  const handleContentClick = (e: MouseEvent) => {
+    e.stopPropagation();
+  };
+
+  const handleClose = useCallback(() => {
+    if (onClose) {
+      setIsClosing(true);
+      timeoutRef.current = window.setTimeout(() => {
+        onClose();
+        setIsClosing(false);
+      }, ANIMATION_DURATION);
+    }
+  }, [onClose]);
+
+  const handleKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      if (e.key === 'Escape') {
+        handleClose();
+      }
+    },
+    [handleClose]
+  );
+
+  useEffect(() => {
+    if (isOpen) {
+      window.addEventListener('keydown', handleKeyDown);
+    }
+
+    return () => {
+      if (timeoutRef.current) {
+        window.clearTimeout(timeoutRef.current);
+        window.removeEventListener('keydown', handleKeyDown);
+      }
+    };
+  }, [isOpen, handleKeyDown]);
+
+  const mods: Record<string, boolean> = {
+    [cls.open]: isOpen,
+    [cls.isClosing]: isClosing
+  };
+
+  return (
+    <div className={cx(cls.modal, mods, [className])}>
+      <div
+        className={cls.overlay}
+        onClick={handleClose}
+        onKeyDown={handleClose}
+        role="button"
+        tabIndex={0}
+      >
+        <div className={cls.content} onClick={handleContentClick}>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
