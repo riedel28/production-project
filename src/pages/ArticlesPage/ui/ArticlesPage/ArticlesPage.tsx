d4e1d0c6e0158cb444b213af3958ca743442fc87
Sergey Riedel <sergey.riedel@gmail.com>
2023-03-13 19:35:45 +0100

    fix: fix lint errors

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -4,18 +4,14 @@ import { memo } from 'react';
 import cls from './ArticlesPage.module.scss';
 
 interface ArticlesPageProps {
-    className?: string;
+  className?: string;
 }
 
 const ArticlesPage = (props: ArticlesPageProps) => {
-    const { className } = props;
-    const { t } = useTranslation();
+  const { className } = props;
+  const { t } = useTranslation();
 
-    return (
-        <div className={classNames(cls.ArticlesPage, {}, [className])}>
-            ARTICLES PAGE
-        </div>
-    );
+  return <div className={classNames(cls.ArticlesPage, {}, [className])}>123</div>;
 };
 
 export default memo(ArticlesPage);
