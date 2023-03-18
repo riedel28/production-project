ac5e264561a49f3616ca5c9100f8dcd791dcae79
Sergey Riedel <sergey.riedel@gmail.com>
2023-03-18 13:43:13 +0100

    fix: fix indentations

--- src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
+++ src/entities/Article/ui/ArticleDetails/ArticleDetails.tsx
@@ -37,16 +37,15 @@ export const ArticleDetails = memo((props: ArticleDetailsProps) => {
   const error = useSelector(getArticleDetailsError);
 
   const renderBlock = useCallback((block: ArticleBlock) => {
-    // eslint-disable
     switch (block.type) {
-      case ArticleBlockType.CODE:
-        return <ArticleCodeBlockComponent key={block.id} block={block} className={cls.block} />;
-      case ArticleBlockType.IMAGE:
-        return <ArticleImageBlockComponent key={block.id} block={block} className={cls.block} />;
-      case ArticleBlockType.TEXT:
-        return <ArticleTextBlockComponent key={block.id} className={cls.block} block={block} />;
-      default:
-        return null;
+    case ArticleBlockType.CODE:
+      return <ArticleCodeBlockComponent key={block.id} block={block} className={cls.block} />;
+    case ArticleBlockType.IMAGE:
+      return <ArticleImageBlockComponent key={block.id} block={block} className={cls.block} />;
+    case ArticleBlockType.TEXT:
+      return <ArticleTextBlockComponent key={block.id} className={cls.block} block={block} />;
+    default:
+      return null;
     }
   }, []);
 
