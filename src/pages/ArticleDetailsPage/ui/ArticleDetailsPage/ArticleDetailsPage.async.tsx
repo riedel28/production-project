7abeba4b84db951547d13f670b6a20a1d927dea7
Sergey Riedel <sergey.riedel@gmail.com>
2023-03-25 12:27:33 +0100

    feat: add fetching articles and view selector

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.async.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.async.tsx
@@ -3,7 +3,6 @@ import { lazy } from 'react';
 export const ArticleDetailsPageAsync = lazy(() => {
   return new Promise((resolve) => {
     // @ts-ignore
-    // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ КУРСА!
-    setTimeout(() => resolve(import('./ArticleDetailsPage')), 1500);
+    setTimeout(() => resolve(import('./ArticleDetailsPage')), 400);
   });
 });
