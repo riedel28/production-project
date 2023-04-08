5cf0fbc47374815b7b2a6b340990ada07aad5f27
Sergey Riedel <sergey.riedel@gmail.com>
2023-04-08 09:45:12 +0200

    refactor: refactor dropdown with headless-ui

--- src/entities/Country/ui/CountrySelect/CountrySelect.tsx
+++ src/entities/Country/ui/CountrySelect/CountrySelect.tsx
@@ -38,7 +38,7 @@ export const CountrySelect = memo(
         items={options}
         readonly={readonly}
         className={className}
-        direction="top"
+        direction="top right"
       />
     );
   }
--- src/shared/ui/ListBox/ListBox.tsx
+++ src/shared/ui/ListBox/ListBox.tsx
@@ -1,6 +1,7 @@
-import { Fragment, ReactNode, useState } from 'react';
+import { Fragment, ReactNode } from 'react';
 import { Listbox as HListBox } from '@headlessui/react';
-import { classNames, Mods } from 'shared/lib/classNames/classNames';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { DropdownDirection } from 'shared/types/ui';
 import { HStack } from '../Stack';
 import { Button } from '../Button/Button';
 import cls from './ListBox.module.scss';
@@ -10,8 +11,6 @@ export interface ListBoxItem {
   content: ReactNode;
   disabled?: boolean;
 }
-
-type DropdownDirection = 'top' | 'bottom';
 
 interface ListBoxProps {
   items?: ListBoxItem[];
@@ -23,10 +22,11 @@ interface ListBoxProps {
   direction?: DropdownDirection;
   label?: string;
 }
-
 const mapDirectionClass: Record<DropdownDirection, string> = {
-  bottom: cls.optionsBottom,
-  top: cls.optionsTop
+  'bottom left': cls.optionsBottomLeft,
+  'bottom right': cls.optionsBottomRight,
+  'top right': cls.optionsTopRight,
+  'top left': cls.optionsTopLeft
 };
 
 export function ListBox(props: ListBoxProps) {
@@ -37,7 +37,7 @@ export function ListBox(props: ListBoxProps) {
     defaultValue,
     onChange,
     readonly,
-    direction = 'bottom',
+    direction = 'bottom right',
     label
   } = props;
 
