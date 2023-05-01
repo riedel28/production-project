c0da3fee50df8ab7fc5e51e97bd38f7ad5f5887a
Sergey Riedel <sergey.riedel@gmail.com>
2023-05-01 18:22:20 +0200

    chore: change select types

--- src/entities/Article/ui/ArticleSortSelector/ArticleSortSelector.tsx
+++ src/entities/Article/ui/ArticleSortSelector/ArticleSortSelector.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next';
-import { memo, useCallback, useMemo } from 'react';
+import { memo, useMemo } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { Select, SelectOption } from '@/shared/ui/Select';
 import { SortOrder } from '@/shared/types';
@@ -7,66 +7,62 @@ import { ArticleSortField } from '../../model/consts/articleConsts';
 import cls from './ArticleSortSelector.module.scss';
 
 interface ArticleSortSelectorProps {
-    className?: string;
-    sort: ArticleSortField;
-    order: SortOrder;
-    onChangeOrder: (newOrder: SortOrder) => void;
-    onChangeSort: (newSort: ArticleSortField) => void;
+  className?: string;
+  sort: ArticleSortField;
+  order: SortOrder;
+  onChangeOrder: (newOrder: SortOrder) => void;
+  onChangeSort: (newSort: ArticleSortField) => void;
 }
 
 export const ArticleSortSelector = memo((props: ArticleSortSelectorProps) => {
-  const {
-    className, onChangeOrder, onChangeSort, order, sort
-  } = props;
+  const { className, onChangeOrder, onChangeSort, order, sort } = props;
   const { t } = useTranslation();
 
-  const orderOptions = useMemo<SelectOption[]>(() => [
-    {
-      value: 'asc',
-      content: t('возрастанию')
-    },
-    {
-      value: 'desc',
-      content: t('убыванию')
-    }
-  ], [t]);
+  const orderOptions = useMemo<SelectOption<SortOrder>[]>(
+    () => [
+      {
+        value: 'asc',
+        content: t('возрастанию')
+      },
+      {
+        value: 'desc',
+        content: t('убыванию')
+      }
+    ],
+    [t]
+  );
 
-  const sortFieldOptions = useMemo<SelectOption[]>(() => [
-    {
-      value: ArticleSortField.CREATED,
-      content: t('дате создания')
-    },
-    {
-      value: ArticleSortField.TITLE,
-      content: t('названию')
-    },
-    {
-      value: ArticleSortField.VIEWS,
-      content: t('просмотрам')
-    }
-  ], [t]);
-
-  const changeSortHandler = useCallback((newSort: string) => {
-    onChangeSort(newSort as ArticleSortField);
-  }, [onChangeSort]);
-
-  const changeOrderHandler = useCallback((newOrder: string) => {
-    onChangeOrder(newOrder as SortOrder);
-  }, [onChangeOrder]);
+  const sortFieldOptions = useMemo<SelectOption<ArticleSortField>[]>(
+    () => [
+      {
+        value: ArticleSortField.CREATED,
+        content: t('дате создания')
+      },
+      {
+        value: ArticleSortField.TITLE,
+        content: t('названию')
+      },
+      {
+        value: ArticleSortField.VIEWS,
+        content: t('просмотрам')
+      }
+    ],
+    [t]
+  );
 
   return (
     <div className={classNames(cls.ArticleSortSelector, {}, [className])}>
-      <Select
+      <Select<ArticleSortField>
         options={sortFieldOptions}
         label={t('Сортировать ПО')}
         value={sort}
-        onChange={changeSortHandler}
+        onChange={onChangeSort}
       />
       <Select
         options={orderOptions}
         label={t('по')}
         value={order}
-        onChange={changeOrderHandler}
+        onChange={onChangeOrder}
         className={cls.order}
       />
     </div>
--- src/shared/ui/Select/Select.tsx
+++ src/shared/ui/Select/Select.tsx
@@ -1,27 +1,27 @@
-import { ChangeEvent, memo, useMemo } from 'react';
+import { ChangeEvent, useMemo } from 'react';
 import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 import cls from './Select.module.scss';
 
-export interface SelectOption {
-  value: string;
+export interface SelectOption<T extends string> {
+  value: T;
   content: string;
 }
 
-interface SelectProps {
+interface SelectProps<T extends string> {
   className?: string;
   label?: string;
-  options?: SelectOption[];
-  value?: string;
-  onChange?: (value: string) => void;
+  options?: SelectOption<T>[];
+  value?: T;
+  onChange?: (value: T) => void;
   readonly?: boolean;
 }
 
-export const Select = memo((props: SelectProps) => {
+export const Select = <T extends string>(props: SelectProps<T>) => {
   const { className, label, options, onChange, value, readonly } = props;
 
   const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
     if (onChange) {
-      onChange(e.target.value);
+      onChange(e.target.value as T);
     }
   };
 
@@ -40,14 +40,9 @@ export const Select = memo((props: SelectProps) => {
   return (
     <div className={classNames(cls.Wrapper, mods, [className])}>
       {label && <span className={cls.label}>{`${label}>`}</span>}
-      <select
-        disabled={readonly}
-        className={cls.select}
-        value={value}
-        onChange={onChangeHandler}
-      >
+      <select disabled={readonly} className={cls.select} value={value} onChange={onChangeHandler}>
         {optionsList}
       </select>
     </div>
   );
-});
+};
