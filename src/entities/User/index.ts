b23a017a324806a0dd86d744344b0251c006f03e
Sergey Riedel <sergey.riedel@gmail.com>
2023-06-13 19:20:32 +0200

    refactor: init auth data from server

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -13,3 +13,4 @@ export type { UserSchema, User } from './model/types/user';
 export { UserRole } from './model/consts/userConsts';
 export { useJsonSettings } from './model/selectors/jsonSettings';
 export { saveJsonSettings } from './model/services/saveJsonSettings';
+export { initAuthData } from './model/services/initAuthData';
