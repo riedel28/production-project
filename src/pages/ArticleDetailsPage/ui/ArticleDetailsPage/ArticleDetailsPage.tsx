020010680026348f272f6a727f885b62ab12c425
Sergey Riedel <sergey.riedel@gmail.com>
2023-06-21 18:45:28 +0200

    chore: move ui components to 'deprecated' folder

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -8,7 +8,7 @@ import {
   ReducersList
 } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { Page } from '@/widgets/Page';
-import { VStack } from '@/shared/ui/Stack';
+import { VStack } from '@/shared/ui/deprecated/Stack';
 import { ArticleRecommendationsList } from '@/features/articleRecommendationsList';
 import { ArticleDetailsComments } from '../ArticleDetailsComments/ArticleDetailsComments';
 import cls from './ArticleDetailsPage.module.scss';
@@ -16,7 +16,7 @@ import { articleDetailsPageReducer } from '../../model/slices';
 import { ArticleDetailsPageHeader } from '../ArticleDetailsPageHeader/ArticleDetailsPageHeader';
 import { ArticleRating } from '@/features/articleRating';
 import { ToggleFeatures } from '@/shared/lib/features';
-import { Card } from '@/shared/ui/Card';
+import { Card } from '@/shared/ui/deprecated/Card';
 
 interface ArticleDetailsPageProps {
   className?: string;
--- src/shared/ui/Stack/VStack/VStack.tsx
+++
@@ -1,8 +0,0 @@
-import { Flex, FlexProps } from '../Flex/Flex';
-
-type VStackProps = Omit<FlexProps, 'direction'>;
-
-export const VStack = (props: VStackProps) => {
-  const { align = 'start', ...restProps } = props;
-  return <Flex direction="column" align={align} {...restProps} />;
-};
