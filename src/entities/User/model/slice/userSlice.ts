df9d97f49808716542d39fbabd861b81162574ee
Sergey Riedel <sergey.riedel@gmail.com>
2023-07-16 17:25:43 +0200

    feat: add app loader layout

--- src/entities/User/model/slice/userSlice.ts
+++ src/entities/User/model/slice/userSlice.ts
@@ -1,5 +1,8 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { USER_LOCALSTORAGE_KEY } from '@/shared/const/localstorage';
+import {
+    LOCAL_STORAGE_LAST_DESIGN_KEY,
+    USER_LOCALSTORAGE_KEY
+} from '@/shared/const/localstorage';
 import { UserSchema, User } from '../types/user';
 import { setFeatureFlags } from '@/shared/lib/features';
 import { saveJsonSettings } from '../services/saveJsonSettings';
@@ -7,22 +10,26 @@ import { JsonSettings } from '../types/jsonSettings';
 import { initAuthData } from '../services/initAuthData';
 
 const initialState: UserSchema = {
-    _inited: false,
+    _inited: false
 };
 
 export const userSlice = createSlice({
     name: 'user',
     initialState,
     reducers: {
-        setAuthData: (state, action: PayloadAction<User>) => {
-            state.authData = action.payload;
-            setFeatureFlags(action.payload.features);
-            localStorage.setItem(USER_LOCALSTORAGE_KEY, action.payload.id);
+        setAuthData: (state, { payload }: PayloadAction<User>) => {
+            state.authData = payload;
+            setFeatureFlags(payload.features);
+            localStorage.setItem(USER_LOCALSTORAGE_KEY, payload.id);
+            localStorage.setItem(
+                LOCAL_STORAGE_LAST_DESIGN_KEY,
+                payload.features?.isAppRedesigned ? 'new' : 'old'
+            );
         },
         logout: (state) => {
             state.authData = undefined;
             localStorage.removeItem(USER_LOCALSTORAGE_KEY);
-        },
+        }
     },
     extraReducers: (builder) => {
         builder.addCase(
@@ -31,7 +38,7 @@ export const userSlice = createSlice({
                 if (state.authData) {
                     state.authData.jsonSettings = payload;
                 }
-            },
+            }
         );
         builder.addCase(
             initAuthData.fulfilled,
@@ -39,12 +46,12 @@ export const userSlice = createSlice({
                 state.authData = payload;
                 setFeatureFlags(payload.features);
                 state._inited = true;
-            },
+            }
         );
         builder.addCase(initAuthData.rejected, (state) => {
             state._inited = true;
         });
-    },
+    }
 });
 
 // Action creators are generated for each case reducer function
